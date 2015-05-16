0afd41dec73e7d80e549fbb02631b18d99606391
LachlanNXT <lachlan-27@hotmail.com>
2015-05-17 00:58:23 +1000

    sensor testing

--- MyCode/Battery/Battery.ino
+++ MyCode/Battery/Battery.ino
@@ -1,16 +1,3 @@
-#include <ZumoBuzzer.h>
-#include <ZumoMotors.h>
-#include <Pushbutton.h>
-#include <QTRSensors.h>
-#include <ZumoReflectanceSensorArray.h>
-#include <avr/pgmspace.h>
-#include <Wire.h>
-#include <LSM303.h>
-
-ZumoMotors motors;
-#define LED 13
-Pushbutton button(ZUMO_BUTTON); // pushbutton on pin 12
-
 int sensorpin = 4;                 // analog pin used to connect the sharp sensor
 int val = 0;                 // variable to store the values from sensor(initially zero)
 float distance = 0; 
@@ -19,19 +6,15 @@ float voltage = 0;
 void setup()
 {
   Serial.begin(9600);               // starts the serial monitor
-  motors.flipLeftMotor(true);
-  motors.flipRightMotor(true);
 }
  
 void loop()
 {
-
-  //while(button.isPressed()){}
   
-  //for (int i = 0; i<3; i++)
-  //{
+  for (int i = 0; i<6; i++)
+  {
   val = analogRead(sensorpin);   // reads the value of the sharp sensor
-  voltage = val*(5/1023.0);
+  voltage = (val/1023) * 5;
   distance = 27.0570*pow(voltage,-1.1811);//pow(((val*(5/1023.0)*0.001221)/16.251),1.1765);
   Serial.print("Sensor ");
   Serial.print(sensorpin);
@@ -41,13 +24,7 @@ void loop()
   Serial.print(voltage);
   Serial.print(" distance ");
   Serial.println(distance);            // prints the value of the sensor to the serial monitor
-  delay(100);                    // wait for this much time before printing next value
-  //}
-  if (distance > 50) {
-    motors.setSpeeds(200, 200);
+  delay(1000);                    // wait for this much time before printing next value
   }
-  else {
-    motors.setSpeeds(-100,-300);
-  }
-  
+
 }
